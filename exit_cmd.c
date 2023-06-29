9b57b2a5e3d166fcc51cd50bdd378d99124b122e
OHind <ouchani.hind@gmail.com>
2023-06-29 15:07:59 -0700

    Some testing

--- exit_cmd.c
+++ exit_cmd.c
@@ -9,7 +9,14 @@
 */
 void exit_cmd(char **command, char *line)
 {
+	int n;
+	n = _atoi(command[1]);
+	if (n <= -1)
+		n = 2;
+	else
+		n = 0;
+
 	free(line);
 	free_buffers(command);
-	exit(2);
+	exit(n);
 }
