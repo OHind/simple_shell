19332325d6c960582da40ed393dd55554409e4a9
OHind <ouchani.hind@gmail.com>
2023-06-29 17:18:32 -0700

    still some tests

--- shell.c
+++ shell.c
@@ -1,89 +1,198 @@
 #include "shell.h"
+#include <stdio.h>
+
+static void handlerc(int);
+void mv_cmd(cmdnode **, int);
+int exec_cmd(char **, int, alias **);
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
+ * main - entry point for simple shell
+ * @argc: argument counter
+ * @argv: argument vector
+ * @argp: env variables
+ *
+ * Return: always 0
  */
-void sig_handler(int sig_num)
+
+int main(int argc, char **argv, char **argp)
 {
-	if (sig_num == SIGINT)
+	char *cmd = NULL, **cmds = NULL;
+	size_t cmdlen = 0;
+	int mode = !isatty(0), i = 0, cmdnum = 1, inputs = 0, ret;
+	alias *head = NULL;
+
+	print_error(argv[0], &inputs, NULL);
+	environ = _arrdup(argp);
+	if (argc > 1)
 	{
-		_puts("\n#cisfun$ ");
+		ret = runscript(argv[1]);
+		free_pp(environ);
+		exit(ret);
 	}
-}
-
-/**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
- */
-void _EOF(int len, char *buff)
-{
-	(void)buff;
-	if (len == -1)
+	signal(SIGINT, handlerc);
+	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		cmd = NULL, cmds = NULL, get_input(&cmd, &cmdlen, &cmds, 0);
+		cmdnum = _arlen(cmds), i = 0;
+		free(cmd);
+		inputs++;
+		while (i < cmdnum)
+			ret = exec_cmd(cmds, i, &head), i += 1;
+		if (cmdnum > 0)
+			free_pp(cmds);
+		if (mode)
 		{
-			_puts("\n");
-			free(buff);
+			free_pp(environ), freealias(head);
+			return (ret);
 		}
-		exit(0);
 	}
+	free_pp(environ), freealias(head);
+	return (ret);
 }
-/**
-  * _isatty - verif if terminal
-  */
 
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
 /**
- * main - Shell
- * Return: 0 on success
+ * handlerc - handlce SIGNUM
+ * @signum: sig ID
  */
 
-int main(void)
+static void handlerc(int signum)
 {
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
+	write(1, "\n$ ", 3);
+	(void)signum;
+}
 
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
+/**
+ * mv_cmd - move to next command in list
+ * @head: first node
+ * @childstat: status of command executed
+ *
+ * Return: none
+ */
+
+void mv_cmd(cmdnode **head, int childstat)
+{
+	cmdnode *tmp = NULL;
+
+	if (!_strcmp((*head)->op, "||"))
 	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
+		if (!childstat)
+		{
+			free((*head)->op);
+			(*head)->op = (*head)->next->op;
+
+			tmp = (*head)->next->next;
+
+			free((*head)->next->cmd);
+			free((*head)->next);
+			(*head)->next = tmp;
+			(*head)->estat = 1;
+		}
+		else
+			(*head) = (*head)->next;
+
+	}
+	else if (!_strcmp((*head)->op, "&&"))
+	{
+		if (!childstat)
+			(*head)->estat = 1, (*head) = (*head)->next;
 		else
 		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
+			while ((*head))
 			{
-				free(buff);
-				f(arv);
-			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
-			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
+				if (!((*head)->next))
+				{
+					*head = (*head)->next;
+					break;
+				}
+				if (!_strcmp((*head)->next->op, "||"))
+				{
+					(*head) = (*head)->next;
+					break;
+				}
+				*head = (*head)->next;
 			}
 		}
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
-	return (0);
+	else
+		(*head) = (*head)->next;
+}
+
+/**
+ * exec_cmd - execute command
+ * @cmd_l: commands
+ * @index: current command index
+ * @aliashead: head of the laias list
+ *
+ * Return: 0 or exit status
+ */
+
+int exec_cmd(char **cmd_l, int index, alias **aliashead)
+{
+	char **tmp = NULL, *cmds = cmd_l[index];
+	int childid, *binstat;
+	static int childstat, exitstat;
+	cmdnode *head = NULL, *_head = NULL, *tmp_n = NULL;
+
+	write_history(cmds);
+	_head = build_list(cmds), head = _head;
+	while (head)
+	{
+		xpnd_str(&(head->cmd), exitstat);
+		xpnd_alias(&(head->cmd), *aliashead);
+		parse_args(head->cmd, " ", &tmp, 0);
+		binstat = handle_bin(tmp, aliashead);
+		if (!binstat[0])
+		{
+			if (binstat[1] != 266)
+			{
+				exitstat = binstat[1];
+				free_pp(environ), free(binstat), free_pp(tmp);
+				free_pp(cmd_l), free_list(_head), freealias(*aliashead);
+				exit(exitstat);
+			}
+			mv_cmd(&head, 0), free(binstat);
+			continue;
+		}
+		free(binstat);
+		if (head->estat == 0)
+		{
+			childid = fork();
+			if (childid < 0)
+			{
+				print_error(NULL, NULL, "Coudn't create a child proccess");
+				exit(127);
+			}
+			if (childid == 0)
+			{
+				childstat = execute(tmp);
+				free_pp(environ), freealias(*aliashead);
+				free_pp(tmp), free_pp(cmd_l), free_list(_head);
+				exit(childstat);
+			}
+			else
+			{
+				wait(&childstat);
+				if (WIFEXITED(childstat))
+					exitstat = WEXITSTATUS(childstat);
+				mv_cmd(&head, exitstat);
+			}
+		}
+		else if (head->estat == 1)
+		{
+			if (!_strcmp(head->op, "||"))
+			{
+				free(head->op);
+				head->op = head->next->op;
+				tmp_n = head->next->next;
+				free(head->next->cmd);
+				free(head->next);
+				head->next = tmp_n;
+			}
+			else
+				head = head->next;
+		}
+		free_pp(tmp);
+	}
+	free_list(_head);
+	return (exitstat);
 }
--- shell.h
+++ shell.h
@@ -1,67 +1,125 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <stddef.h>
 #include <sys/stat.h>
+
+#include <unistd.h>
 #include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
+#define BUFFER_t 1024
 
-char **splitstring(char *str, const char *delim);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/**
+ * struct cmdnode - a node for one command in a command list
+ * @cmd: command string
+ * @op: operation between current command and next command
+ * @estat: exit status (shows if the command has been excuted or not)
+ * @next: a pointer to the next operand
+ */
 
+typedef struct cmdnode
+{
+	char *cmd;
+	char *op;
+	int estat;
+	struct cmdnode *next;
+} cmdnode;
+
+/**
+ * struct alias - a node for one alias
+ * @key: the alais
+ * @value: value of the alias
+ * @next: a pointer to the next alias
+ */
+
+typedef struct alias
+{
+	char *key;
+	char *value;
+	struct alias *next;
+} alias;
+
+
+void mv_cmd(cmdnode **, int);
+int exec_cmd(char **, int, alias **);
+
+
+char *get_file_path(char *);
+void parse_args(char *, const char *, char ***, int);
+int get_input(char **, size_t *, char ***, int);
+void trim_spaces(char **, char *);
+
+int *handle_bin(char **, alias **);
+
+void free_pp(char **);
+int _arlen(char **);
+void *_malloc(unsigned int);
+void *_realloc(void *, unsigned int);
+
+char **_arrdup(char **);
+void printdp(char **);
+
+int _getline(char **, size_t *, int);
+int _strcmpd(char *, const char *);
+char *_strtok(char *, const char *, int);
+int _strcmps(char *, const char *);
+
+int find_tok_occ(char *, char *);
+int find_n_rep(char **, char *, char *);
+
+int _strlen(const char *);
+char *_strcat(char *, char *);
+char *_strdup(char *);
+int _strcmp(char *, char *);
+char *_strcpy(char *, char *);
+
+int _putenv(char *);
+int _setenv(const char *, const char *, int);
+int _unsetenv(const char *);
+char *_getenv(const char *);
+void _printenv(void);
+
+int _chdir(char *);
+int runscript(char *);
+int execute(char **);
+
+int xpnd_str(char **, int);
+
+int add_alias(alias **, char *, char *);
+int print_lalias(alias *);
+int print_alias(alias *, char *);
+int handle_alias(char **, alias **);
+int freealias(alias *);
+
+void xpnd_alias(char **, alias *);
+
+int remove_alias(alias **, char *);
+
+int add_node(cmdnode **, char *, char *);
+cmdnode *build_list(char *);
+int print_nodes(cmdnode *);
+void free_list(cmdnode *);
+cmdnode *add_node_index(cmdnode **, char *, char *, int);
+
+int pow_b(unsigned int base, int power);
+int num_len(unsigned int num);
+char *itoa(int);
+int _atoi(char *);
+
+void print_help(char *arg);
+
+int print_history(void);
+int write_history(char *);
+
+int _write(int, char *, int);
+
+void print_error(char *, int *, char *);
 
 extern char **environ;
-
-/**
- * struct list_path - Linked list containing PATH directories
- * @dir: directory in path
- * @p: pointer to next node
- */
-typedef struct list_path
-{
-	char *dir;
-	struct list_path *p;
-} list_path;
-
-
-char *_getenv(const char *name);
-list_path *add_node_end(list_path **head, char *str);
-list_path *linkpath(char *path);
-char *_which(char *filename, list_path *head);
-
-/**
- * struct mybuild - pointer to function with corresponding buildin command
- * @name: buildin command
- * @func: execute the buildin command
- */
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
-
-void(*checkbuild(char **arv))(char **arv);
-int _atoi(char *s);
-void exitt(char **arv);
-void env(char **arv);
-void _setenv(char **arv);
-void _unsetenv(char **arv);
-
-void freearv(char **arv);
-void free_list(list_path *head);
-
 
 #endif
